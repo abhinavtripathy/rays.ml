59eb3450fe6a6f75a82b42a63152ffe6e9adc586
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-09-07 18:32:57 -0400

    AutoML

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,58 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+const automl = require('@google-cloud/automl');
+const fs = require('fs');
+
+  // Create client for prediction service.
+  const client = new automl.PredictionServiceClient();
+
+  /**
+   * TODO(developer): Uncomment the following line before running the sample.
+   */
+
+  const projectId = "pennapps-xx-252213";
+  const computeRegion = "us-central1";
+  const modelId = "ICN7679781988763224795";
+  const filePath = "./xray.png";
+
+    const scoreThreshold = "0.1";
+
+  // Get the full path of the model.
+  const modelFullId = client.modelPath(projectId, computeRegion, modelId);
+
+  // Read the file content for prediction.
+  const content = fs.readFileSync(filePath, 'base64');
+
+  const params = {};
+
+  if (scoreThreshold) {
+    params.score_threshold = scoreThreshold;
+  }
+
+  // Set the payload by giving the content and type of the file.
+  const payload = {};
+  payload.image = {imageBytes: content};
+
+  // params is additional domain-specific parameters.
+  // currently there is no additional parameters supported.
+  
+  async function test() {
+    const [response] = await client.predict({
+        name: modelFullId,
+        payload: payload,
+        params: params,
+      });
+
+    console.log(`Prediction results:`);
+    response.payload.forEach(result => {
+    console.log(`Predicted class name: ${result.displayName}`);
+    console.log(`Predicted class score: ${result.classification.score}`);
+  });
+  }
+  
+  test();
+  
+
